a5fed8c8a9e2bae5ff01c538e5eb20eda54b02b9
Kunall Banerjee <yeskunall96@gmail.com>
2017-03-28 22:52:48 -0400

    Make zap into a UMD module.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "rules": {
+      "no-console": "off",
+      "quotes": "off",
+      "no-underscore-dangle": "off"
+    }
 };
--- zap.js
+++ zap.js
@@ -1,6 +1,27 @@
-function zap() {
-  const exp = {
-    init: () => console.log(`⚡ zap init!`),
+(function zap(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    console.error(`⚡ zap requires a browser environment to run!`);
+    process.exit(1);
+  } else {
+    // in this case, root is actually window
+    root.zap = factory();
+  }
+}(this, () => {
+  function apiSupport() {
+    return navigator.getBattery;
+  }
+
+  if (!apiSupport()) {
+    console.log(`Your browser doesn't support the BatteryStatus API! 😦`);
+  }
+
+  function _init() {
+    console.log(`⚡ zap init!`);
+  }
+
+  return {
+    init: _init,
   };
-  return exp;
-}
+}));
